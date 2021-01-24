9921817c9b47db0e98d82c3f804549c8f66eef4a
psick22 <kgirok@gmail.com>
2021-01-25 02:45:40 +0900

    회원가입 Form 기능 및 유효성 체크 추가

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 import './RegisterPage.css';
 
 export default function RegisterPage() {
   const { register, handleSubmit, watch, errors } = useForm();
-  const onSubmit = data => {
-    console.log(data);
-  }; // your form submit function which will invoke after successful validation
+  const password = useRef();
+  password.current = watch('password');
 
-  console.log(watch('example')); // you can watch individual input by pass the name of the input
+  const onSubmit = data => console.log(data);
+
+  console.log(watch('password')); // you can watch individual input by pass the name of the input
 
   return (
     <div className='auth-wrapper'>
@@ -20,15 +21,54 @@ export default function RegisterPage() {
           </h6>
           <h3 style={{ marginBottom: '35px' }}>Create your account</h3>
         </div>
+
         <label>Email address</label>
-        <input name='email' type='email' />
+        <input //
+          name='email'
+          type='email'
+          ref={register({ required: true, pattern: /^\S+@\S+$/i })}
+        />
+        {errors.email && <p>이메일은 필수 입력 항목입니다.</p>}
+
         <label>Name</label>
-        <input name='name' />
-        {errors.exampleRequired && <p>This field is required</p>}
+        <input //
+          name='name'
+          ref={register({ required: true, maxLength: 10 })}
+        />
+        {errors.name?.type === 'required' && (
+          <p> 이름은 필수 입력 항목입니다. </p>
+        )}
+        {errors.name?.type === 'maxLength' && (
+          <p> 이름은 10글자를 초과할 수 없습니다. </p>
+        )}
         <label>Password</label>
-        <input name='password' type='password' />
+        <input //
+          name='password'
+          type='password'
+          ref={register({ required: true, minLength: 6 })}
+        />
+        {errors.password?.type === 'required' && (
+          <p> 비밀번호는 필수 입력 항목입니다. </p>
+        )}
+        {errors.password?.type === 'minLength' && (
+          <p> 비밀번호는 최소 6글자 이상이어야 합니다. </p>
+        )}
         <label>Password confirm</label>
-        <input name='password_confirm' type='password' />
+        <input //
+          name='password_confirm'
+          type='password'
+          ref={register({
+            required: true,
+            validate: value => value === password.current,
+          })}
+        />
+        {errors.password_confirm?.type === 'required' && (
+          <p> 비밀번호 확인은 필수 입력 항목입니다. </p>
+        )}
+        {errors.password_confirm?.type === 'validate' && (
+          <p> 비밀번호 확인은 비밀번호와 일치해야 합니다. </p>
+        )}
+
         <input
           style={{ fontWeight: 'bold', textDecoration: 'none' }}
           type='submit'
