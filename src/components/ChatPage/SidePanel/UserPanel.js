26135637e2cf3062489518440c5e3f1b8261640e
psick22 <kgirok@gmail.com>
2021-01-27 20:11:18 +0900

    기능추가: 프로필 이미지 수정(스토리지, DB, 리덕스)

--- src/components/ChatPage/SidePanel/UserPanel.js
+++ src/components/ChatPage/SidePanel/UserPanel.js
@@ -1,17 +1,55 @@
 import { Dropdown } from 'react-bootstrap';
-import React from 'react';
+import React, { useRef } from 'react';
 import { IoIosChatboxes } from 'react-icons/io';
 import Image from 'react-bootstrap/Image';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import firebase from 'firebase';
+import { setPhotoURL } from '../../../redux/actions/user_action';
 
 function UserPanel() {
   const user = useSelector(state => state.user.currentUser);
-  // function handleLogout() {
-  //   firebase.auth().signOut();
-  // }
+
+  const dispatch = useDispatch();
+
+  const inputImageRef = useRef(null);
+  const storageRef = firebase.storage().ref();
+
   const handleLogout = () => {
     firebase.auth().signOut();
+  };
+
+  const handleProfileChange = () => {
+    inputImageRef.current.click();
+  };
+
+  const handleUploadImage = async e => {
+    const files = e.target.files[0];
+    console.log(files.type);
+    const metadata = { contentType: files.type };
+
+    try {
+      let uploadTaskSnapshot = await storageRef
+        .child(`user_images/${user.uid}`)
+        .put(files, metadata);
+      console.log('upload done', uploadTaskSnapshot);
+
+      let path = await uploadTaskSnapshot.ref.getDownloadURL();
+      // auth 유저 프로필 수정
+      const currentUser = firebase.auth().currentUser;
+
+      await currentUser.updateProfile({
+        photoURL: path,
+      });
+      // 리덕스 수정
+      dispatch(setPhotoURL(path));
+
+      // 데이터 베이스 수정
+      await firebase.database().ref('users').child(user.uid).update({
+        image: path,
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -36,7 +74,16 @@ function UserPanel() {
           </Dropdown.Toggle>
 
           <Dropdown.Menu>
-            <Dropdown.Item href='#/action-1'>프로필 사진 변경</Dropdown.Item>
+            <Dropdown.Item onClick={handleProfileChange}>
+              프로필 사진 변경
+            </Dropdown.Item>
+            <input //
+              ref={inputImageRef}
+              style={{ display: 'none' }}
+              type='file'
+              accept='image/jpeg, image/png'
+              onChange={handleUploadImage}
+            />
             <Dropdown.Item onClick={handleLogout}>로그아웃</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
--- src/redux/reducers/user_reducer.js
+++ src/redux/reducers/user_reducer.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { CLEAR_USER, SET_USER } from '../actions/types';
+import { CLEAR_USER, SET_PHOTO_URL, SET_USER } from '../actions/types';
 
 const initialUserState = {
   currentUser: null,
@@ -18,6 +18,12 @@ export default function (state = initialUserState, action) {
       return {
         ...state,
         currentUser: null,
+        isLoading: false,
+      };
+    case SET_PHOTO_URL:
+      return {
+        ...state,
+        currentUser: { ...state.currentUser, photoURL: action.payload },
         isLoading: false,
       };
     default:
