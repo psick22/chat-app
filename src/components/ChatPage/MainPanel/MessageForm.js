4ff871b21915c9ca1c2c1f1bc2fe0c3f1cfce106
psick22 <kgirok@gmail.com>
2021-01-31 04:08:13 +0900

    기능추가: 파일업로드 및 storage에 저장 기능

--- src/components/ChatPage/MainPanel/MessageForm.js
+++ src/components/ChatPage/MainPanel/MessageForm.js
@@ -7,34 +7,115 @@ function MessageForm() {
   const [isValid, setIsValid] = useState(false);
   const [text, setText] = useState('');
   const [loading, setLoading] = useState(false);
-  const textInput = useRef();
+  const [fileLoading, setFileLoading] = useState(false);
+  const [errors, setErrors] = useState([]);
+  const [fileUrl, setFileUrl] = useState('');
+  const textInput = useRef(null);
+  const fileInputRef = useRef(null);
+
   const currentChatRoom = useSelector(state => state.chatRoom.currentChatRoom);
   const currentUser = useSelector(state => state.user.currentUser);
-
+  const storageRef = firebase.storage().ref();
   const messagesRef = firebase.database().ref('messages');
 
-  const handleSubmit = e => {
-    // e.preventDefault();
-    let timestamp = Math.floor(new Date().getTime() / 1000);
-    messagesRef.child(`${currentChatRoom.id}/users/${currentUser.uid}`).update({
+  const createMessage = (fileUrl = null) => {
+    let timestamp = new Date().getTime();
+    const userInfo = {
       user: {
+        uid: currentUser.uid,
         displayName: currentUser.displayName,
         image: currentUser.photoURL,
       },
-    });
-    const messagekey = messagesRef.child(`${currentChatRoom.id}/message`).push()
-      .key;
-    messagesRef.child(`${currentChatRoom.id}/message/${messagekey}`).update({
+    };
+    const message = {
       userUid: currentUser.uid,
       userDisplayName: currentUser.displayName,
       timestamp: timestamp,
       content: text,
-    });
+    };
 
-    setText('');
-    textInput.current.focus();
-    setIsValid(false);
+    if (fileUrl !== null) {
+      message['files'] = fileUrl;
+    }
+
+    return { userInfo, message };
   };
+
+  const handleSubmit = async () => {
+    setLoading(true);
+
+    try {
+      const { userInfo, message } = createMessage();
+      const messagekey = await messagesRef
+        .child(`${currentChatRoom.id}/message`)
+        .push().key;
+
+      await messagesRef
+        .child(`${currentChatRoom.id}/users/${currentUser.uid}`)
+        .set(userInfo);
+
+      await messagesRef
+        .child(`${currentChatRoom.id}/message/${messagekey}`)
+        .update(message);
+
+      setLoading(false);
+      setText('');
+      setIsValid(false);
+      setErrors([]);
+      textInput.current.focus();
+    } catch (error) {
+      setErrors(prevState => prevState.concat(error.message));
+      console.log(error);
+    }
+  };
+
+  const handleUploadClick = e => {
+    console.log(e);
+    fileInputRef.current.click();
+  };
+
+  const onInputChange = async e => {
+    try {
+      setFileLoading(true);
+      console.log(e);
+      // NOTE storage에 올리고 다운로드 url 받아오기
+      // TODO 파일명에 확장자 달아줘서 다운로드 받으면 바로 열수 있게
+      if (!e.target.files[0]) {
+        console.log('undefine filed');
+      } else {
+        let timestamp = new Date().getTime();
+        const files = e.target.files && e.target.files[0];
+        console.log(files);
+        const metadata = {
+          contentType: files.type,
+        };
+
+        let uploadFileSnapshot = await storageRef
+          .child(`files/${currentChatRoom.id}/${timestamp}`)
+          .put(files, metadata);
+        console.log('upload done', uploadFileSnapshot);
+
+        let path = await storageRef
+          .child(`files/${currentChatRoom.id}/${timestamp}`)
+          .getDownloadURL();
+
+        // TODO 받아온 url 채팅창에 파일이름으로 입력하고 (TODO)링크 (이게되나?)
+
+        setFileUrl(path);
+        console.log(path);
+        const newText = text + files.name;
+        console.log(newText);
+        setText(newText);
+        setIsValid(true);
+      }
+      setFileLoading(false);
+    } catch (error) {
+      setErrors(prevState => prevState.concat(error.message));
+      console.log(error);
+      setFileLoading(false);
+    }
+  };
+
   const handleChange = e => {
     if (e.target.value) {
       setIsValid(true);
@@ -79,16 +160,43 @@ function MessageForm() {
       <ProgressBar style={{ marginBottom: '1rem' }} now={45} />
       <Row style={{ marginBottom: '1rem' }}>
         <Col>
-          <Button
-            onClick={handleSubmit}
-            disabled={isValid ? false : true}
-            style={{ width: '100%' }}
-          >
-            SEND
-          </Button>
+          {/* isUpload */}
+          {fileLoading ? (
+            <Button //
+              disabled
+              style={{ width: '100%' }}
+            >
+              UPLOADING...
+            </Button>
+          ) : (
+            <Button //
+              onClick={e => handleUploadClick(e)}
+              style={{ width: '100%' }}
+            >
+              UPLOAD
+              <input
+                ref={fileInputRef}
+                type='file'
+                style={{ display: 'none' }}
+                onChange={onInputChange}
+              />
+            </Button>
+          )}
         </Col>
         <Col>
-          <Button style={{ width: '100%' }}>UPLOAD</Button>
+          {loading ? (
+            <Button disabled style={{ width: '100%' }}>
+              LOADING
+            </Button>
+          ) : (
+            <Button
+              onClick={handleSubmit}
+              disabled={isValid ? false : true}
+              style={{ width: '100%' }}
+            >
+              SEND
+            </Button>
+          )}
         </Col>
       </Row>
     </div>
